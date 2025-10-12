9842d5d52d866deae6a6b88a0a9f86f32fc05a71
Marina Tcai <tcaimarina@gmail.com>
2025-10-12 19:19:38 +0200

    add bird of prey hero

--- heroes-app/app/page.tsx
+++ heroes-app/app/page.tsx
@@ -6,10 +6,11 @@ export default function Home() {
     <h2 className="text-center text-5xl text-white font-outline-2 p-5 m-10 alfa-slab">Heroes.</h2>
     <div className="text-left text-4xl text-white font-outline-2 p-5 m-10 alfa-slab border-2 rounded-[0px_50px_50px_50px] border-solid"> Your heroes.</div>
     <div className="text-right text-4xl text-white font-outline-2 p-5 m-10 alfa-slab border-2 rounded-[50px_0px_50px_50px] border-solid"> Your choice.</div>
-    <div className="flex justify-between items-center">
+     
+    <div className="flex justify-between items-center my-15">
       <div className=" bg-white rounded-[0px_20px_20px_0px] border-solid border-black border-2">
         <h2 className="text-6xl text-[#6192D3] p-3 m-5">Jusctice League.</h2>
-        <p className="text-xl text-[#474747] p-5 m-5">The team of heroes consists of the strongest DC superheroes who only come together when there is a truly major threat to fight.</p>
+        <p className=" text-left text-xl text-[#474747] p-5 m-5 mr-25">The team of heroes consists of the strongest DC superheroes who only come together when there is a truly major threat to fight.</p>
       </div>
       <div className="h-1 w-20 bg-white"></div>
       <div className="flex justify-center">
@@ -18,6 +19,21 @@ export default function Home() {
           src="justice_league.png" 
           alt="Justice_league"></img>
         </div>
+      </div>
+    </div>
+
+    <div className="flex justify-between items-center my-15">
+      <div className="flex justify-center">
+        <div className="bg-white rounded-2xl h-42 w-42 ml-5">
+          <img className="h-42 w-42 center p-3"
+          src="bird_of_prey.png" 
+          alt="bird_of_prey"></img>
+        </div>
+      </div>
+      <div className="h-1 w-20 bg-white"></div>
+      <div className=" bg-white rounded-[20px_0px_0px_20px] border-solid border-black border-2">
+        <h2 className="text-right text-6xl text-[#E85C6A] p-3 m-5">Birds of Prey.</h2>
+        <p className="text-right text-xl text-[#474747] p-5 m-5 ml-25">The all-female team appeared alongside Harley Quinn in Birds of Prey: The Emancipation of Harley Quinn. Teaming up with the Joker&apos;s crazy ex-girlfriend, the squad took on the villain Black Mask.</p>
       </div>
     </div>
     </>
