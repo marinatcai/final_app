49f2eaef991329e92b8083a3dbfbf4d0efad531e
Marina Tcai <tcaimarina@gmail.com>
2025-10-14 23:05:03 +0200

    build vote component

--- heroes-app/app/components/vote-item.tsx
+++ heroes-app/app/components/vote-item.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 interface VoteItemProps {
     title:string;
     image:string
@@ -7,13 +8,12 @@ export default function VoteItem({title, image}: VoteItemProps) {
 
     return (
         <>
-        <div className="flex justify-center">
-            <img src="" alt="">
+        <div className="flex flex-col justify-items items-center">
+            <img className="h-32 w-32 p-3" src={image} alt="Image_of_hero">
             </img>
-            <label></label>
-            <input></input>
+            <label className="text-center text-2xl text-white font-outline-2 alfa-slab p-1">{title}</label>
+            <input name="hero" type="radio" className="h-5 w-5 cursor-pointer rounded-full" id={title}></input>
         </div>
-
         </>
     );
 }
--- heroes-app/app/vote/page.tsx
+++ heroes-app/app/vote/page.tsx
@@ -1,28 +1,43 @@
 "use client"
 
+import Link from "next/link";
 import VoteItem from "../components/vote-item";
 
 export default function VotePage() {
     const items =[
-      {name: "Magic", image:"poison.gif"},
-      {name: "Poison", image:"poison.gif"},
-      {name: "Health", image:"health-elexir.gif"},
+      {name: "Jusctice League", image:"justice_league.png"},
+      {name: "Birds of Prey", image:"bird_of_prey.png"},
+      {name: "Suicide Squad", image:"blade.png"},
     ]
+
+    function vote() {
+        // here i want to return the choice and show it in console log
+
+         // here i want link to next page your info
+        <Link href="/your_info" >
+        </Link>
+        // here i want link to next page your info
+    }
+
+// set Vote title to the left
 
     return (
         <>
-            <h2>Vote.</h2>
-                <div className="flex flex-col justify-items-center">   
-                    <div className="bg-white h-100 w-150">
-                        <h2>Which squad shall win?</h2>
-
-                        <div className="grid grid-cols-3 gap-2 mb-10">
-                            {items.map((item, index) => (
-                                <VoteItem image={item.image} title={item.name} key={index} />
-                            ))}
-                        </div>
+            <div className="flex flex-col justify-items items-center">  
+                <h2 className="p-3 text-4xl text-white font-outline-2 alfa-slab">Vote.</h2>
+                <div className="bg-white h-80 w-200 rounded-2xl drop-shadow-black drop-shadow-2xl mb-10">
+                    <h2 className="text-center p-6 text-3xl text-white font-outline-2 alfa-slab">Which squad shall win?</h2>
+                    <div className="flex justify-evenly mx-10 mb-5">
+                        {items.map((item, index) => (
+                            <VoteItem image={item.image} title={item.name} key={index} />
+                        ))}
                     </div>
                 </div>
+                <button className=" bg-[#F86C6F]  h-12 w-48 text-white text-3xl font-outline-2 alfa-slab cursor-pointer rounded-xl border-3 border-white"
+                onClick={vote}>
+                Continue
+                </button>
+            </div>
         </>
     );
 }
