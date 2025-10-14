58f1a17a186237981fdfe1579d0540f2ab3e09e2
Marina Tcai <tcaimarina@gmail.com>
2025-10-14 23:09:13 +0200

    fix position of the section vote

--- heroes-app/app/vote/page.tsx
+++ heroes-app/app/vote/page.tsx
@@ -23,7 +23,7 @@ export default function VotePage() {
 
     return (
         <>
-            <div className="flex flex-col justify-items items-center">  
+            <div className="flex flex-col justify-items items-center mt-20">  
                 <h2 className="p-3 text-4xl text-white font-outline-2 alfa-slab">Vote.</h2>
                 <div className="bg-white h-80 w-200 rounded-2xl drop-shadow-black drop-shadow-2xl mb-10">
                     <h2 className="text-center p-6 text-3xl text-white font-outline-2 alfa-slab">Which squad shall win?</h2>
